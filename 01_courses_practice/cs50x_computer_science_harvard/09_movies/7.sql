77341c5d0eb11fa323ec44dac79972d99653eb56
Elisei <pro.elisei@gmail.com>
2025-09-07 17:13:35 +0300

    cs50x projects and exercises

--- 01_courses_practice/cs50x_computer_science_harvard/09_movies/7.sql
+++
@@ -1,5 +0,0 @@
-SELECT movies.title, ratings.rating
-FROM movies
-JOIN ratings ON ratings.movie_id = movies.id
-WHERE movies.year=2010 AND ratings.rating IS NOT NULL
-ORDER BY ratings.rating DESC, movies.title;
